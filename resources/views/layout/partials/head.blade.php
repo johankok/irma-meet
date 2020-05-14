6916a645481a4b8361545fe6dda10126364e9d11
Confiks <confiks@scriptbase.org>
2020-05-14 13:41:52 +0200

    chore: Clean up <head>, Javascript and SCSS file

--- resources/views/layout/partials/head.blade.php
+++ resources/views/layout/partials/head.blade.php
@@ -1,21 +1,10 @@
 <meta charset="utf-8">
 
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
 <meta name="description" content="">
-
 <meta name="author" content="">
 
-<title>IRMA-meet the secure video chat</title>
-
-<!-- Bootstrap core CSS -->
-
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+<title>IRMA-meet authenticated video chatting</title>
 
 <link href="https://fonts.googleapis.com/css?family=Roboto:300,400" rel="stylesheet">
-
-
-
-<!-- Custom styles for this template -->
-
-<link href="{{ url('/') }}/css/custom.css" rel="stylesheet">
+<link href="{{ url('/') }}/css/app.css" rel="stylesheet">
